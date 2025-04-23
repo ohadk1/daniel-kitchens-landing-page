c721ccb814264a9b31c490a59d7647b154b41bd7
ohadk1 <kienanohad3@gmail.com>
2025-04-23 20:11:21 +0300

    grid

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -40,7 +40,7 @@ const Gallery = () => {
         </p>
 
         {/* הגריד ל-8 פרויקטים */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {kitchenProjects.map((project, index) => (
             <div
               key={project.id}
